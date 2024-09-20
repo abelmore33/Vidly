cfe8c487d9df072a949cc3935c2784035aa264e5
belmoreaaron33 <belmoreaaron9@gmail.com>
2024-09-20 01:11:38 -0400

    Added colormode sswitch

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import  React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import ReactDOM from 'react-dom/client'
-import App from './App';
+import React from "react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import theme from "./components/Theme";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
